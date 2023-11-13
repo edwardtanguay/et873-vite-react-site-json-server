12255a89c8305686b6c99f98eb8e289afe9aacb9
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-13 12:03:53 +0100

    can save employees in json

--- src/pages/PageSimpleForm.tsx
+++ src/pages/PageSimpleForm.tsx
@@ -1,11 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FormEvent } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const PageSimpleForm = () => {
+	const navigate = useNavigate();
+
 	const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		const formData = new FormData(event.target as HTMLFormElement);
-const data = Object.fromEntries(formData.entries());
-console.log(data);
+		const employee = Object.fromEntries(formData);
+		const headers = {
+			"Access-Control-Allow-Origin": "*",
+			"Content-Type": "application/json",
+		};
+
+		(async () => {
+			try {
+				const response = await axios.post(
+					"http://localhost:3021/employees",
+					employee,
+					{headers}
+				);
+				if (response.status === 201) {
+					navigate('/info');
+				} else {
+					console.log(`ERROR: ${response.status}`);
+				}
+			} catch (error: any) {
+				console.log(`ERROR: ${error.message}`);
+			}
+		})();
 	};
 	return (
 		<form onSubmit={handleFormSubmit} encType="multipart/form-data">
@@ -30,35 +55,35 @@ console.log(data);
 					</label>
 					<input type="text" id="lastName" name="lastName" required />
 				</div>
-                <div className="mb-4 flex gap-2">
-                    <label className="w-[10rem]" htmlFor="hireDate" >
-                        Hire Date:
-                    </label>
-                    <input type="date" id="hireDate" name="hireDate" required />
-                </div>
- 
-                <div className="mb-4 flex gap-2">
-                    <label className="w-[10rem]" htmlFor="employeeNumber">
-                        Employee Number:
-                    </label>
-                    <div>
-                        <input
-                            type="text"
-                            id="employeeNumber"
-                            name="employeeNumber"
-                            pattern="^[DSM]-\d{4}$"
-                            required
-                        />
-                        <p className="text-sm">e.g. D-2832, S-7771, M-8123</p>
-                    </div>
-                </div>
- 
-                <div className="mb-4 flex gap-2">
-                    <label className="w-[10rem]" htmlFor="notes" >
-                        Notes:
-                    </label>
-                    <textarea id="notes" name="notes"></textarea>
-                </div>
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="hireDate">
+						Hire Date:
+					</label>
+					<input type="date" id="hireDate" name="hireDate" required />
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="employeeNumber">
+						Employee Number:
+					</label>
+					<div>
+						<input
+							type="text"
+							id="employeeNumber"
+							name="employeeNumber"
+							pattern="^[DSM]-\d{4}$"
+							required
+						/>
+						<p className="text-sm">e.g. D-2832, S-7771, M-8123</p>
+					</div>
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="notes">
+						Notes:
+					</label>
+					<textarea id="notes" name="notes"></textarea>
+				</div>
 				<div className="mt-5 flex justify-end pr-3">
 					<button>Add Employee</button>
 				</div>
